6e178774df3a5fb776272c421b46e7eb043ed071
unknown <Pobarul-islam>
2022-05-22 10:33:35 +0600

    banner and card set

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link, Router } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
+
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home Page</h2>
+        <div className='px-12'>
+            <Banner></Banner>
+            <Info></Info>
+
         </div>
     );
 };
