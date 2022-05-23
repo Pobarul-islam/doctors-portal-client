75a13eb1b789e88456b3ad5f727d074ae23814e2
unknown <Pobarul-islam>
2022-05-23 20:03:39 +0600

    login and signup from authication setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
 import Appoinment from './Pages/Appoinment/Appoinment';
+import Signup from './Pages/Login/Signup';
 
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/appoinment' element={<Appoinment></Appoinment>}></Route>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/appointment' element={<Appoinment></Appoinment>}></Route>
       </Routes>
     </div>
   );
