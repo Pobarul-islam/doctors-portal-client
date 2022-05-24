1a8d92cea2a6946dc81d992e99415cd800e02b0f
unknown <Pobarul-islam>
2022-05-24 11:25:13 +0600

    booking add to backend and react query added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
 import Appoinment from './Pages/Appoinment/Appoinment';
 import Signup from './Pages/Login/Signup';
+import RequireAuth from './Pages/Login/RequireAuth';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -17,8 +20,13 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
-        <Route path='/appointment' element={<Appoinment></Appoinment>}></Route>
+        <Route path='/appointment' element={
+          <RequireAuth>
+            <Appoinment></Appoinment>
+          </RequireAuth>
+        }></Route>
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
